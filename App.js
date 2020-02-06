4bff998a5ed9419a3e98d18fae629cedd90ca843
Dibakar mandal <dibakar.redbulls@gmail.com>
2020-02-06 11:14:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+//import FormsAndInputs from './Components/FormsAndInputs';
+//import ValidationForm from './Components/ValidationForm';
+import Test from './Components/Test';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ValidationForm/> */}
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
+//<FormsAndInputs/>
